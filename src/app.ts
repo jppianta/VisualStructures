0e16259e6c0a34fba497e0807ba3c350fb7e89e5
jppianta <joao.pianta@outlook.com>
2018-05-26 12:24:53 -0300

    Design update

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,42 @@
 export class App {
-  styleObject: object; 
+  styleVisualizer: object; 
+  styleEditor:object
+  styleContainer:object;
+  styleLogo: object;
+  styleLogoDiv: object;
 
   constructor() {
-    this.styleObject = {
-      'width': '50%',
-      'height': '98vh'
+    this.styleVisualizer = {
+      'width': '100%',
+      'height': '87.6vh',
+      'border-style': 'solid',
+      'border-color': 'rgba(0, 0, 0, 0.07)'
+    }
+
+    this.styleEditor = {
+      'width': '100%',
+      'height': '87.6vh',
+      'border-bottom': 'solid',
+      'border-top': 'solid',
+      'border-color': 'rgba(0, 0, 0, 0.07)'
+    }
+
+    this.styleContainer = {
+      display: 'grid',
+      'grid-template-columns': '50% 50%'
+    }
+
+    this.styleLogoDiv = {
+      width: '100%',
+      height: '10vh'
+    }
+
+    this.styleLogo = {
+        'width': 'auto',
+        'height': '90%',
+        'display': 'block',
+        'margin-left': 'auto',
+        'margin-right': 'auto'
     }
   }
 }
---
+++ src/visualizer/visualizer.ts
@@ -0,0 +1,69 @@
+import { Network } from "vis/index";
+
+export class Visualizer {
+    styleObject: object;
+    ele: HTMLElement;
+    nodes;
+    edges;
+    options;
+    network;
+
+    constructor() {
+        this.styleObject = {
+            'width': '100%',
+            'height': '100%' 
+        }
+    }
+
+    createData() {
+        this.edges = [];
+        this.nodes = [];
+        for (var i = 0; i < 15; i++) {
+            this.nodes.push({id: i, label: String(i)});
+        }
+        this.edges.push({from: 0, to: 1});
+        this.edges.push({from: 0, to: 6});
+        this.edges.push({from: 0, to: 13});
+        this.edges.push({from: 0, to: 11});
+        this.edges.push({from: 1, to: 2});
+        this.edges.push({from: 2, to: 3});
+        this.edges.push({from: 2, to: 4});
+        this.edges.push({from: 3, to: 5});
+        this.edges.push({from: 1, to: 10});
+        this.edges.push({from: 1, to: 7});
+        this.edges.push({from: 2, to: 8});
+        this.edges.push({from: 2, to: 9});
+        this.edges.push({from: 3, to: 14});
+        this.edges.push({from: 1, to: 12});
+        this.nodes[0]["level"] = 0;
+        this.nodes[1]["level"] = 1;
+        this.nodes[2]["level"] = 3;
+        this.nodes[3]["level"] = 4;
+        this.nodes[4]["level"] = 4;
+        this.nodes[5]["level"] = 5;
+        this.nodes[6]["level"] = 1;
+        this.nodes[7]["level"] = 2;
+        this.nodes[8]["level"] = 4;
+        this.nodes[9]["level"] = 4;
+        this.nodes[10]["level"] = 2;
+        this.nodes[11]["level"] = 1;
+        this.nodes[12]["level"] = 2;
+        this.nodes[13]["level"] = 1;
+        this.nodes[14]["level"] = 5;
+
+        this.options = {
+            physics: false,
+            autoResize: true
+        }
+
+        return {
+            nodes: this.nodes,
+            edges: this.edges
+        }
+    }
+
+    attached() {
+        this.ele = document.getElementById('visu');
+        this.network = new Network(this.ele, this.createData(), this.options);
+    }
+}
