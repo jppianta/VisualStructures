29789bbf3e467f9e8dbac088cb42351c62c4f3ab
jppianta <jppianta83@Joaos-Mac-mini.local>
2018-09-24 22:25:35 -0300

    new Tabs class

---
+++ src/Tabs.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+export class Tabs extends Component {
+    constructor(props) {
+        super(props);
+
+        this.tabs = [];
+        (props.children || []).forEach(tab => {
+            if (tab.type === 'tab') {
+                this.tabs.push({
+                    name: tab.props.name || '',
+                    elements: tab.props.children || []
+                });
+            }
+        });
+        this.state = {
+            visibleTab: this.tabs.length && this.tabs[0].name
+        }
+    }
+
+    onClick = (e) => {
+        this.setState({ visibleTab: e.tab });
+        this.render();
+    }
+
+    render() {
+        return (
+            <div className="Tabs">
+                <div className='TabButtons'>
+                    {
+                        this.tabs.map(tab => {
+                            return <button key={tab.name} className={this.state.visibleTab === tab.name ? 'active' : 'notActive'} onClick={this.onClick.bind(this, { tab: tab.name })}>{tab.name}</button>
+                        })
+                    }
+                </div>
+                <div className='TabContents'>
+                    {
+                        this.tabs.map(tab => {
+                            return (
+                                <div key={tab.name} style={this.state.visibleTab === tab.name ? {} : {display: 'none'}}>
+                                    {tab.elements}
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        );
+    }
+}
