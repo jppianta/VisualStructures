1a4ff902a2c319e97dd96ba6c5558c514662dcf6
jppianta <jppianta83@Joaos-Mac-mini.local>
2018-11-06 22:12:58 -0200

    new logos and color

--- src/bars/Header.js
+++ src/bars/Header.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import logo from '../images/logo.svg';
+import newLogo from '../images/newLogo.svg';
 
 export class Header extends Component {
     render() {
         return (
             <div className='header'>
-                <img src={logo} alt=''></img>
+                <img src={newLogo} alt=''></img>
             </div>
         );
     }
