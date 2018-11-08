34f7c0d7f24197e7686e4c2a196289b7a0b3597b
jppianta <jppianta83@Joaos-Mac-mini.local>
2018-11-07 22:10:28 -0200

    new poster

--- poster/poster.js
+++ poster/poster.js
@@ -1,10 +1,24 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('http://localhost:8080/poster.html');
-  await page.pdf({path: 'test.pdf', format: 'A4'});
-
-  await browser.close();
+  try {
+    const browser = await puppeteer.launch({headless: true});
+    const page = await browser.newPage();
+    page.setViewport({
+      width: 775,
+      height: 910
+    });
+    await page.addStyleTag({
+      url: 'https://fonts.googleapis.com/css?family=Muli'
+    });
+    await page.addStyleTag({
+      path: './poster.css'
+    });
+    await page.goto('http://localhost:8080/poster.html');
+    await page.screenshot({path: 'example.jpg', type: 'jpeg', quality: 100});
+  
+    await browser.close();
+  } catch (err) {
+    console.error(err);
+  }
 })();
