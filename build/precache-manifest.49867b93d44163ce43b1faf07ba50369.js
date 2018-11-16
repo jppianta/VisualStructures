b6d549c4181c0ee14d0cd731d09aed2d17684ef1
jppianta <jppianta83@Joaos-Mac-mini.local>
2018-11-16 00:56:45 -0200

    new build

---
+++ build/precache-manifest.49867b93d44163ce43b1faf07ba50369.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a247b4ce7e999de4eeeac854abb7d699",
+    "url": "/VisualStructures/static/media/newLogo.a247b4ce.svg"
+  },
+  {
+    "revision": "f260bf7c0dad4d6d2581",
+    "url": "/VisualStructures/static/js/runtime~main.f260bf7c.js"
+  },
+  {
+    "revision": "b15a910ab99273e88ff6",
+    "url": "/VisualStructures/static/js/main.b15a910a.chunk.js"
+  },
+  {
+    "revision": "698a63ef30f4522001ef",
+    "url": "/VisualStructures/static/js/1.698a63ef.chunk.js"
+  },
+  {
+    "revision": "b15a910ab99273e88ff6",
+    "url": "/VisualStructures/static/css/main.0b13f1de.chunk.css"
+  },
+  {
+    "revision": "698a63ef30f4522001ef",
+    "url": "/VisualStructures/static/css/1.172c1eed.chunk.css"
+  },
+  {
+    "revision": "36ca661c0be7dddca6576cde4923407b",
+    "url": "/VisualStructures/index.html"
+  }
+];
